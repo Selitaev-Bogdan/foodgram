694e72ce89c7b9e7c055132b8602eaeff1a86836
Богдан Селитаев <selitaevbogdan@gmail.com>
2026-05-22 18:47:38 +0800

    Add pagination to RecipeViewSet1

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,6 +1,8 @@
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
-from .models import Ingredient, Recipe, RecipeIngredient, Tag
+from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                     ShoppingCart, Tag)
 from backend.users.serializers import UserSerializer
 
 
@@ -32,14 +34,34 @@ class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
     author = UserSerializer(read_only=True)
     ingredients = RecipeIngredientSerializer(
-        many=True,
-        read_only=True,
-        source='recipe_ingredients'
+        many=True, read_only=True, source='recipe_ingredients'
     )
+    image = Base64ImageField()
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
 
     class Meta:
         model = Recipe
         fields = (
             'id', 'author', 'name', 'image', 'text',
-            'cooking_time', 'tags', 'ingredients'
+            'cooking_time', 'tags', 'ingredients',
+            'is_favorited', 'is_in_shopping_cart'
         )
+
+    def get_is_favorited(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Favorite.objects.filter(user=user, recipe=obj).exists()
+
+    def get_is_in_shopping_cart(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return ShoppingCart.objects.filter(user=user, recipe=obj).exists()
+
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
